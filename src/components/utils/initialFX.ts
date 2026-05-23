41f2b0f588d1c3ad031ff4cc853356b6988f63ee
Your Name <you@example.com>
2026-05-23 19:16:19 +0500

    Simplify landing page to use static text without overlapping

--- src/components/utils/initialFX.ts
+++ src/components/utils/initialFX.ts
@@ -14,7 +14,7 @@ export function initialFX() {
   });
 
   const landingTextElements = document.querySelectorAll(
-    [".landing-info h3", ".landing-intro h2", ".landing-intro h1"].join(",")
+    [".landing-info h3", ".landing-intro h2", ".landing-intro h1", ".landing-info h2"].join(",")
   );
 
   gsap.fromTo(
@@ -31,18 +31,6 @@ export function initialFX() {
   );
 
   gsap.fromTo(
-    ".landing-info-h2",
-    { opacity: 0, y: 30 },
-    {
-      opacity: 1,
-      duration: 1.2,
-      ease: "power1.inOut",
-      y: 0,
-      delay: 0.8,
-    }
-  );
-  
-  gsap.fromTo(
     [".header", ".icons-section", ".nav-fade"],
     { opacity: 0 },
     {
@@ -52,62 +40,4 @@ export function initialFX() {
       delay: 0.1,
     }
   );
-
-  gsap.set(".landing-h2-info-1, .landing-h2-2", { opacity: 0, y: 80 });
-
-  LoopText(".landing-h2-info", ".landing-h2-info-1");
-  LoopText(".landing-h2-1", ".landing-h2-2");
-}
-
-function LoopText(Text1: string, Text2: string) {
-  var tl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
-  const delay = 4;
-  const delay2 = delay * 2 + 1;
-
-  tl.to(
-    Text1,
-    {
-      opacity: 0,
-      y: -80,
-      duration: 1.2,
-      ease: "power3.inOut",
-      delay: delay,
-    },
-    0
-  )
-    .fromTo(
-      Text2,
-      { opacity: 0, y: 80 },
-      {
-        opacity: 1,
-        y: 0,
-        duration: 1.2,
-        ease: "power3.inOut",
-        delay: delay2,
-      },
-      1
-    )
-    .to(
-      Text2,
-      {
-        opacity: 0,
-        y: -80,
-        duration: 1.2,
-        ease: "power3.inOut",
-        delay: delay,
-      },
-      2
-    )
-    .fromTo(
-      Text1,
-      { opacity: 0, y: 80 },
-      {
-        opacity: 1,
-        y: 0,
-        duration: 1.2,
-        ease: "power3.inOut",
-        delay: delay2,
-      },
-      3
-    );
 }
